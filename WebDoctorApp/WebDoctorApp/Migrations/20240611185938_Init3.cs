4fc1ac7518ad4c3201574d60d438b7c330d443d3
abubienczyk <olabubienczyk@gmail.com>
2024-06-11 21:11:28 +0200

    migrations

---
+++ WebDoctorApp/WebDoctorApp/Migrations/20240611185938_Init3.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebDoctorApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DueDate",
+                table: "Prescription",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Prescription",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthday",
+                table: "Patient",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.UpdateData(
+                table: "Patient",
+                keyColumn: "IdPatient",
+                keyValue: 1,
+                column: "Birthday",
+                value: new DateTime(2000, 5, 15, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Patient",
+                keyColumn: "IdPatient",
+                keyValue: 2,
+                column: "Birthday",
+                value: new DateTime(1990, 5, 15, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Prescription",
+                keyColumn: "IdPrescription",
+                keyValue: 1,
+                columns: new[] { "Date", "DueDate" },
+                values: new object[] { new DateTime(2024, 6, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 6, 17, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Prescription",
+                keyColumn: "IdPrescription",
+                keyValue: 2,
+                columns: new[] { "Date", "DueDate" },
+                values: new object[] { new DateTime(2024, 6, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 6, 18, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "DueDate",
+                table: "Prescription",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Date",
+                table: "Prescription",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Birthday",
+                table: "Patient",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.UpdateData(
+                table: "Patient",
+                keyColumn: "IdPatient",
+                keyValue: 1,
+                column: "Birthday",
+                value: new DateOnly(2000, 5, 15));
+
+            migrationBuilder.UpdateData(
+                table: "Patient",
+                keyColumn: "IdPatient",
+                keyValue: 2,
+                column: "Birthday",
+                value: new DateOnly(1990, 5, 15));
+
+            migrationBuilder.UpdateData(
+                table: "Prescription",
+                keyColumn: "IdPrescription",
+                keyValue: 1,
+                columns: new[] { "Date", "DueDate" },
+                values: new object[] { new DateOnly(2024, 6, 10), new DateOnly(2024, 6, 17) });
+
+            migrationBuilder.UpdateData(
+                table: "Prescription",
+                keyColumn: "IdPrescription",
+                keyValue: 2,
+                columns: new[] { "Date", "DueDate" },
+                values: new object[] { new DateOnly(2024, 6, 11), new DateOnly(2024, 6, 18) });
+        }
+    }
+}
